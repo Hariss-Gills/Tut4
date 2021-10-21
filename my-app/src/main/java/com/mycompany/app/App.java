cf6d38415c7c68428ccc06712f053093a65530c5
HARISS ALI GILLS <h.gills.20@abdn.ac.uk>
2021-10-21 12:31:19 +0000

    Complied

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]); // error!
     }
 }
